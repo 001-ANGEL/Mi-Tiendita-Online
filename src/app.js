fe7ab18b68e9e2f2acb8d8033b6cc04188ea3cd7
Angel <isaac11lopez11@gmail.com>
2024-12-19 23:13:14 -0600

    Creacion del CRUD en Productos

--- src/app.js
+++ src/app.js
@@ -1,18 +1,13 @@
 import express from 'express';
-import { connectDB } from './config/db.js';
+import productsRouter from './routes/products.routes.js';
 
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.use(productsRouter);
 
 app.get('/', async(req, res) => {
-    try {
-        const pool = await connectDB();
-        const result = await pool.request().query('SELECT * FROM Usuarios');
-        res.json(result.recordset); 
-    } catch (error) {
-        console.log(error);
-    }
+    res.send('server is running');
 })
 
 app.listen(port, () => {
